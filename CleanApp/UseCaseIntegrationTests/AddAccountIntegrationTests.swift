36f244cfae69675e7a07df21ff255736ffd60a82
luksinocencio <luksinocencio@gmail.com>
2023-09-03 11:58:47 -0300

    add useCaseIntegrationTests

---
+++ CleanApp/UseCaseIntegrationTests/AddAccountIntegrationTests.swift
@@ -0,0 +1,28 @@
+import XCTest
+
+@testable import Data
+@testable import Domain
+@testable import Infra
+
+class AddAccountIntegrationTests: XCTestCase {
+    // TODO: - Usar Api local
+    func test_add_account() {
+        let alamofireAdapter = AlamofireAdapter()
+        // URL fora do ar
+        let url = URL(string: "https://clean-node-api.herokuapp.com/api/signup")!
+        let sut = RemoteAddAccount(url: url, httpClient: alamofireAdapter)
+        let addAccountModel = AddAccountModel(name: "Lucas Inocencio", email: "luksinocencio@outlook.com", password: "secret", passwordConfirmation: "secret")
+        let exp = expectation(description: "waiting")
+        sut.add(addAccountModel: addAccountModel) { result in
+            switch result {
+            case .failure: XCTFail("Expect success got \(result) instead")
+            case .success(let account):
+                XCTAssertNil(account.id)
+                XCTAssertEqual(account.name, addAccountModel.name)
+                XCTAssertEqual(account.email, addAccountModel.email)
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 10)
+    }
+}
