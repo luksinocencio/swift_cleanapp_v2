44339395eb3339741d6eaddcba934df0997f8cf1
luksinocencio <luksinocencio@gmail.com>
2023-09-04 19:52:49 -0300

    alteracao de arquivos

--- CleanApp/Presentation/Presenters/SignUp/SignUpMapper.swift
+++ CleanApp/Presentation/Presenters/SignUp/SignUpMapper.swift
@@ -3,6 +3,11 @@ import Domain
 
 public final class SignUpMapper {
     static func toAddAccountModel(viewModel: SignUpViewModel) -> AddAccountModel {
-        return AddAccountModel(name: viewModel.name!, email: viewModel.email!, password: viewModel.password!, passwordConfirmation: viewModel.passwordConfirmation!)
+        return AddAccountModel(
+            name: viewModel.name!,
+            email: viewModel.email!,
+            password: viewModel.password!,
+            passwordConfirmation: viewModel.passwordConfirmation!
+        )
     }
 }
